8b499595bc9f2367b8bd78a90f14d259243eb084
Jason M <jason.moposita1201@gmail.com>
2024-09-24 13:54:49 -0400

    Simple Working API

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')  // Gives us access to express
+const app = express()   // wherever we see 'app' we're using 'express'
+const PORT = 8000
+
+const rappers = {
+    'bad bunny': {
+        'age': 30,
+        'birthName': 'Benito Antonio Martínez Ocasio',
+        'birthLocation': 'Vega Baja, Puerto Rico'
+    },
+    'myke towers': {
+        'age': 30,
+        'birthName': 'Michael Anthony Torres Monge',
+        'birthLocation': 'Río Piedras, Puerto Rico'
+    },
+    'random': {
+        'age': 0,
+        'birthName': 'Unknown',
+        'birthLocation': 'Unknown'
+    }
+}
+
+// Set up server to hear request and respond with 
+// simple HTML file
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (req, res) => {
+    const rapperName = req.params.name.toLowerCase()
+    if(rappers[rapperName]) {
+        res.json(rappers[rapperName])
+    }else {
+        res.json(rappers['random'])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
